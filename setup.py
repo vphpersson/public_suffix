e4437eeb4d31153feecfca76a4eb6c2f81437a9d
Viktor Persson <vph.persson@gmail.com>
2021-01-09 22:21:59 +0100

    Fix `setup.py`.

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='public_suffix',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+        'pyutils @ git+ssh://git@github.com/vphpersson/pyutils.git#egg=pyutils'
+    ]
 )
